34501c8e04e06df2fd1829679430434b0aca3352
computerextra <81916339+computerextra@users.noreply.github.com>
2023-05-24 10:52:24 +0200

    treesitter removed

--- lua/jay/packer.lua
+++ lua/jay/packer.lua
@@ -20,11 +20,4 @@ return require('packer').startup(function(use)
 
   use "lukas-reineke/indent-blankline.nvim"
 
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	}
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,11 +89,6 @@ _G.packer_plugins = {
     path = "C:\\Users\\johannes.kirchner\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
-  ["nvim-treesitter"] = {
-    loaded = true,
-    path = "C:\\Users\\johannes.kirchner\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
-    url = "https://github.com/nvim-treesitter/nvim-treesitter"
-  },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
