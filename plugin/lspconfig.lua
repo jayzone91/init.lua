8dfa88683c9d2845cf6f8d4232815302b943e90d
jayzone91 <register@j4yz0n3.de>
2023-05-25 18:47:56 +0200

    Update lspconfig.lua

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -18,5 +18,6 @@ end
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" }
+  cmd = { "typescript-language-server", " --stdio" }
 }
+
