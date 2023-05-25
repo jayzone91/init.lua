c71b43d9f029e0fd72ad49c3bb2386afc309a904
jayzone91 <register@j4yz0n3.de>
2023-05-25 20:41:55 +0200

    autopairs + autotag install

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,20 +10,22 @@ packer.startup(function(use)
   use "wbthomason/packer.nvim"
   use {
     "svrana/neosolarized.nvim",
-    requires = { "tjdevries/colorbuddy.nvim"}
+    requires = { "tjdevries/colorbuddy.nvim" }
   }
   use {
     "nvim-lualine/lualine.nvim", -- statusline
     requires = { "nvim-tree/nvim-web-devicons", opt = true }
   }
-  use "onsails/lspkind-nvim" -- VS Code like pictograms
-  use "hrsh7th/cmp-buffer" -- vim-cmp source for buffer words
-  use "hrsh7th/cmp-nvim-lsp" -- nvim-cmp source for neovim`s built-in LSP
-  use "hrsh7th/nvim-cmp" -- Completion
+  use "onsails/lspkind-nvim"  -- VS Code like pictograms
+  use "hrsh7th/cmp-buffer"    -- vim-cmp source for buffer words
+  use "hrsh7th/cmp-nvim-lsp"  -- nvim-cmp source for neovim`s built-in LSP
+  use "hrsh7th/nvim-cmp"      -- Completion
   use "neovim/nvim-lspconfig" -- LSP
   use 'L3MON4D3/LuaSnip'
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate"
   }
+  use "windwp/nvim-autopairs"
+  use "windwp/nvim-ts-autotag"
 end)
