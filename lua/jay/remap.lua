a868f37ab9d272a60787448ab8813c594e200866
jayzone91 <register@j4yz0n3.de>
2023-05-27 20:27:15 +0200

    Alles ganz neu

---
+++ lua/jay/remap.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+-- open File Explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- just to be lazy
+vim.keymap.set("", "<C-c>", "<Esc>")
+
+-- delete a word backward
+vim.keymap.set("n","dw",'vb"_d')
+
+-- Split Screen
+vim.keymap.set("n","sv", ":vsplit<Return><C-w>w", { silent = true })
+vim.keymap.set("n","ss", ":split<Return><C-w>w", { silent = true })
+-- Move to other screen
+vim.keymap.set("","s<up>", "<C-w>k")
+vim.keymap.set("","s<left>", "<C-w>h")
+vim.keymap.set("","s<down>", "<C-w>j")
+vim.keymap.set("","s<right>", "<C-w>l")
+
+-- Move line
+vim.keymap.set("", "<leader><up>", ":m -2<Return>", { silent = true })
+vim.keymap.set("", "<leader><down>", ":m +1<Return>", { silent = true })
+
+-- load file
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end)
+
+-- fun Stuff
+vim.keymap.set("n","<leader>mr", "<cmd>CellularAutomation make_it_rain<CR>");
