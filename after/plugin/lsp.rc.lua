c9a033e78e019b340d0f5053dca133c885a0417b
jayzone91 <register@j4yz0n3.de>
2023-05-27 16:55:47 +0200

    prettier weg

--- after/plugin/lsp.rc.lua
+++ after/plugin/lsp.rc.lua
@@ -11,7 +11,7 @@ lsp.ensure_installed({
 lsp.nvim_workspace()
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -29,15 +29,15 @@ lsp.setup_nvim_cmp({
 lsp.set_preferences({
   suggest_lsp_servers = false,
   sign_icons = {
-      error = 'E',
-      warn = 'W',
-      hint = 'H',
-      info = 'I'
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
   }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -54,5 +54,5 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
--- after/plugin/prettier.rc.lua
+++
@@ -1,7 +0,0 @@
-local status, Neoformat = pcall(require, "neoformat")
-if (not status) then return end
-
-augroup fmt
-  autocmd!
-  autocmd BufWritePre * undojoin | Neoformat
-augroup END
